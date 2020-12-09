0114eba83c985a543b3cd9ff4a2687952b696fd0
Harkness Wolf <ammophila.procera@gmail.com>
2020-12-08 18:27:12 -0600

    Built flowfield

--- script.js
+++ script.js
@@ -3,3 +3,90 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = 550;
 canvas.height = 550;
+
+const maxParticles = 200;
+let particlesArray = [];
+
+squareSize = 50;
+arrayDimensions = canvas.width / squareSize;
+
+const vectorField = new Array(arrayDimensions * arrayDimensions);
+
+class Vector {
+  constructor() {
+    this.theta = Math.random() * Math.PI * 2;
+    // this.magnitude = 1;
+    // this.origin = [0, 0];
+    this.x = Math.cos(this.theta);
+    this.y = Math.sin(this.theta);
+  }
+}
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.radius = 2;
+    this.hue = 'red';
+    this.arrayX = Math.floor(this.x / squareSize);
+    this.arrayY = Math.floor(this.y / squareSize);
+    this.index = this.arrayY * arrayDimensions + this.arrayX;
+  }
+  draw() {
+    ctx.fillStyle = this.hue;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+  }
+  update() {
+    if ((this.x > 0 && this.x < canvas.width) && (this.y > 0 && this.y < canvas.height)) {
+      this.arrayX = Math.floor(this.x / squareSize);
+      this.arrayY = Math.floor(this.y / squareSize);
+      this.index = this.arrayY * arrayDimensions + this.arrayX;
+      let headingX = vectorField[this.index].x;
+      let headingY = vectorField[this.index].y;
+      this.x += headingX;
+      this.y += headingY;
+    }
+  }
+}
+
+function visualizeArray() {
+  for (y = 0; y < arrayDimensions; y++) {
+    for (x = 0; x < arrayDimensions; x++) {
+      ctx.strokeStyle = '#777';
+      ctx.strokeRect(x * squareSize, y * squareSize, squareSize, squareSize);
+    }
+  }
+}
+
+function populateVectorArray() {
+  for (i = 0; i < vectorField.length; i++) {
+    vectorField[i] = new Vector();
+  }
+}
+
+function populateParticleArray() {
+  for (i = 0; i < maxParticles; i++) {
+    particlesArray.push(new Particle);
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update();
+    particlesArray[i].draw();
+  }
+  visualizeArray();
+  requestAnimationFrame(animate);
+}
+
+function init() {
+  populateVectorArray();
+  populateParticleArray();
+  animate();
+}
+
+init();
